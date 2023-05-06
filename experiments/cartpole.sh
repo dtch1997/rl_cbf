8992d060ada29bd42c6abe9a6be9b1b4decf0ea7
Daniel Tan <dtch1997@users.noreply.github.com>
2023-05-06 13:35:01 +0100

    Add bash script to run experiments

---
+++ experiments/cartpole.sh
@@ -0,0 +1,60 @@
+# baseline experiment
+python rl_cbf/learning/dqn_train.py \
+  --env-id DiverseCartPole-v1 \
+  --eval-frequency 10000 \
+  --viz-frequency 100000 \
+  --wandb-entity dtch1997 \
+  --wandb-project RL_CBF \
+  --save-model \
+  --total-timesteps 2000000 \
+  --exploration-fraction 0.125 \
+  --exp-name baseline_2M \
+  --capture-video \
+  --track
+
+# supervised losses
+python rl_cbf/learning/dqn_train.py \
+  --env-id DiverseCartPole-v1 \
+  --eval-frequency 10000 \
+  --viz-frequency 100000 \
+  --wandb-entity dtch1997 \
+  --wandb-project RL_CBF \
+  --save-model \
+  --total-timesteps 2000000 \
+  --exploration-fraction 0.125 \
+  --exp-name baseline_2M \
+  --capture-video \
+  --track \
+  --supervised-loss-coef 1.0
+  
+# bump architecture
+python rl_cbf/learning/dqn_train.py \
+  --env-id DiverseCartPole-v1 \
+  --eval-frequency 10000 \
+  --viz-frequency 100000 \
+  --wandb-entity dtch1997 \
+  --wandb-project RL_CBF \
+  --save-model \
+  --total-timesteps 2000000 \
+  --exploration-fraction 0.125 \
+  --exp-name baseline_2M \
+  --capture-video \
+  --track \
+  --enable-bump-parametrization
+
+# bump architecture with supervised losses
+python rl_cbf/learning/dqn_train.py \
+  --env-id DiverseCartPole-v1 \
+  --eval-frequency 10000 \
+  --viz-frequency 100000 \
+  --wandb-entity dtch1997 \
+  --wandb-project RL_CBF \
+  --save-model \
+  --total-timesteps 2000000 \
+  --exploration-fraction 0.125 \
+  --exp-name baseline_2M \
+  --capture-video \
+  --track \
+  --enable-bump-parametrization \
+  --supervised-loss-coef 1.0
+
