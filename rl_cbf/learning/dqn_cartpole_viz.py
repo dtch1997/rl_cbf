e34a710392422a40aad63bfe0baeca4cad250120
Daniel Tan <dtch1997@users.noreply.github.com>
2023-05-09 13:36:05 +0100

    Fix plotting code

--- rl_cbf/learning/dqn_cartpole_viz.py
+++ rl_cbf/learning/dqn_cartpole_viz.py
@@ -30,7 +30,7 @@ def get_default_spaces():
     Returns:
         Tuple[np.ndarray, np.ndarray]: Tuple of x and theta space
     """
-    x_space = torch.linspace(-3.2, 3.2, steps=100)
+    x_space = torch.linspace(-4, 4, steps=100)
     x_dot_space = torch.linspace(-3.2, 3.2, steps=100)
     theta_space = torch.linspace(-np.pi / 4, np.pi / 4, steps=100)
     theta_dot_space = torch.linspace(-3, 3, steps=100)
@@ -56,11 +56,12 @@ def plot_heatmap_theta(fig: plt.Figure,
     X = theta_space
     Y = theta_dot_space
     Z = barrier_values[0,0].detach().cpu().numpy()
+    # Transpose because contourf is weird
     Z = Z.T
     levels = MaxNLocator(nbins=15).tick_values(Z.min(), Z.max())
     cmap = plt.get_cmap('coolwarm')
     norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
-    image = ax.pcolormesh(X, Y, Z, cmap=cmap, norm=norm)
+    image = ax.contourf(X, Y, Z, cmap=cmap, norm=norm)
     fig.colorbar(image, ax=ax)
     theta_threshold = 12 * 2 * np.pi / 360
     ax.axvline(-theta_threshold, color='k', linestyle='--', linewidth=3)
@@ -80,11 +81,12 @@ def plot_heatmap_x(fig: plt.Figure,
     X = x_space
     Y = x_dot_space
     Z = barrier_values[:,:,0,0].detach().cpu().numpy()
+    # Transpose because contourf is weird
     Z = Z.T
     levels = MaxNLocator(nbins=15).tick_values(Z.min(), Z.max())
     cmap = plt.get_cmap('coolwarm')
     norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
-    image = ax.pcolormesh(X, Y, Z, cmap=cmap, norm=norm)
+    image = ax.contourf(X, Y, Z, cmap=cmap, norm=norm)
     fig.colorbar(image, ax=ax)
     x_threshold = -2.4
     ax.axvline(-x_threshold, color='k', linestyle='--', linewidth=3)
@@ -94,43 +96,6 @@ def plot_heatmap_x(fig: plt.Figure,
     ax.set_ylabel('x_dot')
     
     return ax
-
-def load_model(
-    model_path: str,
-    make_env: Callable,
-    env_id: str,
-    run_name: str,
-    Model: torch.nn.Module,
-    device: torch.device = torch.device("cpu"),
-    capture_video: bool = True,
-):
-    envs = gym.vector.SyncVectorEnv([make_env(env_id, 0, 0, capture_video, run_name)])
-    model = Model(envs).to(device)
-    model.load_state_dict(torch.load(model_path, map_location=device))
-    model.eval()
-    return model, envs
-
-
-def load_ensemble_model(
-    envs: gym.vector.SyncVectorEnv,
-    model_paths: List[str],
-    device: torch.device = torch.device("cpu"),
-):
-    models = []
-    for model_path in model_paths:
-        _model = QNetwork(envs, True).to(device)
-        _model.load_state_dict(torch.load(model_path, map_location=device))
-        models.append(_model)
-    model = QNetworkEnsemble(envs, models).to(device)
-    model.eval()
-    return model
-
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--env-id', type=str)
-    parser.add_argument('--model-paths', type=str, nargs='+')
-    args = parser.parse_args()
-    return args
 
 def get_initial_states(n_samples: int = 1):
     high = np.array([2.0, 0.1, 0.15, 0.1])
@@ -150,7 +115,7 @@ class DQNCartPoleVisualizer:
         _, _, theta_space, theta_dot_space = get_default_spaces()
         x_space, x_dot_space, _, _ = get_zero_spaces()
         grid_x, grid_x_dot, grid_theta, grid_theta_dot = torch.meshgrid(
-            x_space, x_dot_space, theta_space, theta_dot_space, indexing='xy'
+            x_space, x_dot_space, theta_space, theta_dot_space
         )
         states = torch.stack([grid_x, grid_x_dot, grid_theta, grid_theta_dot], dim=-1)
         states = states.reshape(-1, 4)
@@ -167,7 +132,7 @@ class DQNCartPoleVisualizer:
         x_space, x_dot_space, _, _ = get_default_spaces()
         _, _, theta_space, theta_dot_space = get_zero_spaces()
         grid_x, grid_x_dot, grid_theta, grid_theta_dot = torch.meshgrid(
-            x_space, x_dot_space, theta_space, theta_dot_space, indexing='xy'
+            x_space, x_dot_space, theta_space, theta_dot_space
         )
         states = torch.stack([grid_x, grid_x_dot, grid_theta, grid_theta_dot], dim=-1)
         states = states.reshape(-1, 4)
@@ -181,3 +146,33 @@ class DQNCartPoleVisualizer:
             ax[1].scatter(state_history[:,0], state_history[:,1], c='k', s=1)
 
         return fig
+    
+if __name__ == "__main__":
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser = QNetwork.add_argparse_args(parser)
+    parser.add_argument('--model-path', type=str, required=True)
+    args = parser.parse_args()
+
+    model_path = args.model_path
+    envs = gym.vector.SyncVectorEnv([lambda: gym.make('DiverseCartPole-v1')])
+    model = QNetwork.from_argparse_args(envs, args)
+    model.load_state_dict(torch.load(model_path, map_location=args.device))
+    visualizer = DQNCartPoleVisualizer()
+    fig = visualizer.visualize(model)
+    fig.show()
+    input('Press enter to continue...')
+
+    # Sanity check that model outputs are expected
+    print(model.predict_value(np.array([0.0, 0.0, -0.3, 0.0])))
+    print(model.predict_value(np.array([0.0, 0.0, 0.3, 0.0])))
+    print(model.predict_value(np.array([0.0, 0.0, 0.0, -0.3])))
+    print(model.predict_value(np.array([0.0, 0.0, 0.0, 0.3])))
+    print()
+
+    # Sanity check model values
+    print(model.predict_value(np.array([-2.0, 0.0, 0.0, 0])))
+    print(model.predict_value(np.array([2.0, 0.0, 0.0, 0])))
+    print(model.predict_value(np.array([0.0, 2.0, 0.0, 0])))
+    print(model.predict_value(np.array([0.0, -2.0, 0.0, 0])))
