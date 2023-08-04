b9d19b5c39257206769264ded0210bd9f8adc81f
Daniel Tan <dtch1997@users.noreply.github.com>
2023-08-04 21:09:02 +0100

    Add evaluation of CBF on offline policy

--- rl_cbf/offline/td3_bc_eval.py
+++ rl_cbf/offline/td3_bc_eval.py
@@ -4,11 +4,48 @@ from rl_cbf.offline.td3_bc import *
 
 
 @torch.no_grad()
-def eval_cbf():
+def eval_cbf(
+    env: gym.Env, model: TD3_BC, device: str, n_episodes: int, seed: int
+) -> Dict[str, np.ndarray]:
     # TODO: evaluate the CBF for off-policy safety constraints
     # Evaluate: Safety-constrained episode length
     # Evaluate: Safety success rate
     pass
+
+    env.seed(seed)
+    model.set_eval()
+    episode_rewards = []
+    episode_lengths = []
+
+    for _ in range(n_episodes):
+        state, done = env.reset(), False
+        episode_reward = 0.0
+        episode_length = 0
+        while not done:
+            # Take random action
+            action = env.action_space.sample()
+            # Apply safety constraint
+            state_th = torch.from_numpy(state).float().to(device).view(1, -1)
+            action_th = torch.from_numpy(action).float().to(device).view(1, -1)
+            next_q_value = model.get_q_value(state_th, action_th)
+            if next_q_value < 0.5 / (1 - model.discount):
+                # Unsafe; take action that maximizes Q-value
+                action = model.actor.act(state, device)
+            state, reward, done, info = env.step(action)
+            env.render()
+            episode_reward += reward
+            episode_length += 1
+        episode_rewards.append(episode_reward)
+        episode_lengths.append(episode_length)
+
+    episode_safety_successes = np.asarray(episode_lengths) == 1000
+    model.set_train()
+
+    return {
+        "episode_rewards": np.asarray(episode_rewards),
+        "episode_lengths": np.asarray(episode_lengths),
+        "episode_safety_successes": np.asarray(episode_safety_successes),
+    }
 
 
 @torch.no_grad()
@@ -47,9 +84,17 @@ def eval(config: TrainConfig):
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
 
-    # TODO: avoid hardcoding
-    state_mean = np.load(f"state_mean.npy")
-    state_std = np.load(f"state_std.npy")
+    env_type = None
+    env_types = ["ant", "halfcheetah", "hopper", "walker2d"]
+    for env_type in env_types:
+        if env_type in config.env:
+            env_type = env_type
+            break
+    if env_type is None:
+        raise ValueError(f"Unknown env type: {config.env}")
+
+    state_mean = np.load(f"rl_cbf/data/{env_type}_state_mean.npy")
+    state_std = np.load(f"rl_cbf/data/{env_type}_state_std.npy")
     env = wrap_env(env, state_mean=state_mean, state_std=state_std)
 
     max_action = float(env.action_space.high[0])
@@ -97,6 +142,25 @@ def eval(config: TrainConfig):
         trainer.load_state_dict(torch.load(policy_file))
         actor = trainer.actor
 
+    # Evaluate CBF
+    eval_cbf_dict = eval_cbf(
+        env,
+        trainer,
+        device=config.device,
+        n_episodes=config.n_episodes,
+        seed=config.seed,
+    )
+    eval_episode_length = eval_cbf_dict["episode_lengths"].mean()
+    eval_safety_success = eval_cbf_dict["episode_safety_successes"].mean()
+    print("---------------------------------------")
+    print(
+        f"CBF evaluation over {config.n_episodes} episodes: "
+        f"Episode length: {eval_episode_length:.3f}"
+        f" , Safety success: {eval_safety_success:.3f}"
+    )
+    print("---------------------------------------")
+
+    # Evaluate policy
     eval_dict = eval_actor(
         env,
         actor,
