86c9edaf82dfa82fe5a46facd38f333ce7695d33
Daniel Tan <dtch1997@users.noreply.github.com>
2023-08-06 12:52:52 +0100

    Register all d4rl locomotion envs

--- rl_cbf/envs/registration.py
+++ rl_cbf/envs/registration.py
@@ -19,45 +19,29 @@ register(
     entry_point="rl_cbf.envs.safety_env:SafetyWalker2dEnv",
     kwargs={"env_id": "Walker2d-v2"},
     max_episode_steps=1000,
-    reward_threshold=950.0,
 )
 
-register(
-    "Safety-walker2d-medium-v2",
-    entry_point="rl_cbf.envs.safety_env:SafetyWalker2dEnv",
-    kwargs={"env_id": "walker2d-medium-v2"},
-    max_episode_steps=1000,
-    reward_threshold=950.0,
-)
+# Register d4rl envs
 
-register(
-    "Safety-walker2d-expert-v2",
-    entry_point="rl_cbf.envs.safety_env:SafetyWalker2dEnv",
-    kwargs={"env_id": "walker2d-expert-v2"},
-    max_episode_steps=1000,
-    reward_threshold=950.0,
-)
-
-register(
-    "Safety-walker2d-medium-replay-v2",
-    entry_point="rl_cbf.envs.safety_env:SafetyWalker2dEnv",
-    kwargs={"env_id": "walker2d-medium-replay-v2"},
-    max_episode_steps=1000,
-    reward_threshold=950.0,
-)
-
-register(
-    "Safety-ant-medium-v2",
-    entry_point="rl_cbf.envs.safety_env:SafetyAntEnv",
-    kwargs={"env_id": "ant-medium-v2"},
-    max_episode_steps=1000,
-    reward_threshold=950.0,
-)
-
-register(
-    "Safety-hopper-medium-v2",
-    entry_point="rl_cbf.envs.safety_env:SafetyHopperEnv",
-    kwargs={"env_id": "hopper-medium-v2"},
-    max_episode_steps=1000,
-    reward_threshold=950.0,
-)
+for env_type in ("ant", "halfcheetah", "hopper", "walker2d"):
+    for dataset_type in (
+        "random",
+        "medium",
+        "expert",
+        "medium-replay",
+        "medium-expert",
+        "mixed",
+    ):
+        env_id = f"{env_type}-{dataset_type}-v2"
+        env_class_name = "Safety" + env_type.capitalize() + "Env"
+        base_env_id = env_id
+        if dataset_type == "mixed":
+            # The mixed dataset is constructed in the training script
+            # So we set a dummy value for the base env id
+            base_env_id = f"{env_type}-medium-v2"
+        register(
+            f"Safety-{env_id}",
+            entry_point=f"rl_cbf.envs.safety_env:{env_class_name}",
+            kwargs={"env_id": base_env_id},
+            max_episode_steps=1000,
+        )
