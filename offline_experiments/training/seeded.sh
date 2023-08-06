a69aee6196061c26a1a063ead511661125a316bd
Daniel Tan <dtch1997@users.noreply.github.com>
2023-08-06 12:50:47 +0100

    Update settings for seeded runs

--- offline_experiments/training/seeded.sh
+++ offline_experiments/training/seeded.sh
@@ -7,15 +7,25 @@ group_name=${2:-'d4rl_cbf'}
 
 for seed in 1 2 3 4 5
 do 
-    for env in Safety-walker2d-expert-v2 Safety-walker2d-medium-v2 # Safety-ant-medium-v2 Safety-hopper-medium-v2
+    for env in (
+        "Safety-walker2d-random-v2"
+        "Safety-walker2d-expert-v2" 
+        "Safety-walker2d-medium-v2" # Safety-ant-medium-v2 Safety-hopper-medium-v2
+        "Safety-walker2d-mixed-v2"
+    )
     do 
         for relabel in zero_one # constant_0.2 constant_0.8
         do
             if [ $relabel == "zero_one" ]
             then
-                options="--bounded True --safe_supervised True --unsafe_supervised True --detach_actor True"
+                options=" --bounded True --unsafe_supervised True --detach_actor True"
             else
-                options="--bounded False --safe_supervised False --unsafe_supervised False --detach_actor False"
+                options=" --bounded False --unsafe_supervised False --detach_actor False"
+            fi
+
+            if [ $env == "Safety-walker2d-mixed-v2" ]
+            then
+                options+=" --use_mixed_dataset True"
             fi
             
             python rl_cbf/offline/td3_bc_train.py \
@@ -27,6 +37,7 @@ do
                 --name $env-$relabel-seed=$seed \
                 --max_timesteps 500000 \
                 --checkpoints_path models \
+                --buffer_size 3000000 \
                 $options
         done
     done
